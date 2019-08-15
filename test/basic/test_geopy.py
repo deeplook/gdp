7fe2455bb299b40aba4a34cadf30f51b74d2aeed
deeplook <gherman@darwin.in-berlin.de>
2019-08-15 20:50:14 +0200

    Delete test_geopy.py

--- test/basic/test_geopy.py
+++
@@ -1,7 +0,0 @@
-def test_geolocation():
-    "Test geolocation."
-    from geopy.geocoders import Nominatim
-    geolocator = Nominatim(user_agent="test")
-    location = geolocator.geocode("175 5th Avenue NYC")
-    addr = location.address
-    assert addr.startswith('Flatiron Building, 175, 5th Avenue, Flatiron District')
